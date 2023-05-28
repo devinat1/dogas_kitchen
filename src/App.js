ce1e4d3903082545481a9d8d48266b86312b8977
Devin Ersoy <dersoy@purdue.edu>
2023-05-28 17:51:57 +0200

    Added a new favicon

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import logo from "./logo.svg";
 import "./App.css";
 import ListMenu from "./components/ListMenu";
 import MainPage from "./mainPage";
