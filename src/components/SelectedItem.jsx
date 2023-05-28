0d3382494269f0b2af48265794dfab779537e369
Devin Ersoy <dersoy@purdue.edu>
2023-05-28 17:55:42 +0200

    Added back button

--- src/components/SelectedItem.jsx
+++ src/components/SelectedItem.jsx
@@ -1,15 +1,32 @@
+import { useNavigate } from "react-router-dom";
+
 function SelectedItem(props) {
   const { name, description, image, id } = props;
+  const navigate = useNavigate();
+
+  const handleBackClick = () => {
+    navigate(-1);
+  };
 
   return (
     <div className="max-w-2xl mx-auto">
+      <div className="mb-4 rounded-lg shadow-lg max-w-2xl">
+        <img
+          src={image}
+          alt={name}
+          className="max-w-full h-auto border border-gray-300"
+        />
+      </div>
       <h1 className="text-3xl font-bold mb-4">{name}</h1>
-      <img
-        src={image}
-        alt={name}
-        className="mb-4 rounded-lg shadow-lg max-w-full h-auto border border-gray-300"
-      />
       <p className="text-lg mb-4">{description}</p>
+      <div className="flex justify-center">
+        <button
+          onClick={handleBackClick}
+          className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded-full"
+        >
+          Back
+        </button>
+      </div>
     </div>
   );
 }
